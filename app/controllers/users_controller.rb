f2134fa8e82616e0ccb3e2fc19eb959c529df0ed
Elaine Lee <elaneylee@gmail.com>
2015-02-08 15:10:38 -0800

    add templates and shared folders style header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,25 +10,26 @@ class UsersController < ApplicationController
       #     redirect_to user_path
       end
 
-      # def show
-      #   @user = User.find(params[:id])
-      # end
+      def show
+        @user = User.find(params[:id])
+      end
 
-      # def create
-      #   @user = User.new(user_params)
-      #   if @user.save
-      #     log_in @user
-      #     redirect_to user_path
-      # end
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          log_in @user
+          redirect_to user_path
+        end
+      end
 
-      # def destroy
-      # end
+      def destroy
+      end
 
-      # private
+      private
 
-      #   def user_params
-      #     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      #   end
+        def user_params
+          params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
 
    
 end
