5363968c0cb6d8811a69b49c4dc359dffbd3d95c
Elaine Lee <elaneylee@gmail.com>
2015-02-08 15:22:01 -0800

    add sessions controller and helper

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			log_in user
+			redirect_to user_path
+		else
+			flash[:danger] = "Failed to log in"
+			redirect_to root_path
+		end
+
+	end
+
+	def destroy
+		log_out if logged_in?
+		redirect_to root_url
+	end
+
+end
