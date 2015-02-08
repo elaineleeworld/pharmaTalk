fc636685d4d609f809184b735919c7e51017f004
Elaine Lee <elaneylee@gmail.com>
2015-02-08 00:32:56 -0800

    add requirements to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require angular
+//= require angular-resource
+//= require angular-route
+//= require angular-rails-templates
 //= require_tree .
+//= require_tree ./templates
