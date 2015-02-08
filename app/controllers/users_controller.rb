8e4da3a1551331039e7ea90efebda35f8154135c
Elaine Lee <elaneylee@gmail.com>
2015-02-08 00:30:49 -0800

    create users controller remove turbolinks

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+      # def index
+      #   @users = User.all
+      # end
+
+      # def new
+      #   @user = User.new
+      #   if logged_in?
+      #     redirect_to user_path
+      # end
+
+      # def show
+      #   @user = User.find(params[:id])
+      # end
+
+      # def create
+      #   @user = User.new(user_params)
+      #   if @user.save
+      #     log_in @user
+      #     redirect_to user_path
+      # end
+
+      # def destroy
+      # end
+
+      # private
+
+      #   def user_params
+      #     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      #   end
+
+   
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,30 @@
 class User < ActiveRecord::Base
+
+	# a user has many posts and has many comments
+	# if user is deleted, destroy all of that user's posts and comments as well
+	has_many :posts, dependent: :destroy
+	has_many :comments, dependent: :destroy
+
+	# downcase email before saving it
+	before_save :downcase_email
+
+	# check there is a name and max length of 50
+	validates :name, presence: true, length: {maximum: 50}
+
+	# check there is an email, it's format, uniqueness, and max length 250
+	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {case_sensitive: false}, length: {maximum: 255}
+
+    # check password is at least 6 characters long
+	validates :password, length: {minimum: 6}
+
+	# takes care of password encryption and validation 
+	# no need for those methods
+	has_secure_password
+
+	# method to downcase the email
+	private
+		def downcase_email
+			self.email = email.downcase
+		end
+
 end
