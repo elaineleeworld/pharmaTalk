85a6eb5dca0e44e55df0de56af381a68be8d4b64
Elaine Lee <elaneylee@gmail.com>
2015-02-08 16:13:00 -0800

    add namespace api and api folder controllers

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,11 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  namespace :api, defaults: {format: :json} do
+    resources :posts
+    resources :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
