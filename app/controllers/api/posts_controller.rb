477f6653a3481c272749364fd90b92cad29545f2
Elaine Lee <elaneylee@gmail.com>
2015-02-08 16:50:05 -0800

    add api posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,1 +1,49 @@
-posts_controller.rb
+module API
+	class PostsController < ApplicationController
+		protect_from_forgery with: :null_session
+
+		respond_to :html, :xml, :json
+
+		def index
+			respond_with Post.all
+		end
+
+		def show
+			respond_with Post.find(params[:id])
+		end
+
+		def create
+			post = Post.new(post_params)
+			user = User.find(params[:user_id]
+		    post.user = user
+
+		    if post.save
+		    	render json: post, status: 201
+		    else
+		    	render json: {errors: post.errors}, status: 422
+		    end
+		end
+
+		def update
+			post = Post.find(params[:id])
+			if post.update(post_params)
+				render json: post, status: 200
+			else 
+				render json: {errors: post.errors}, status: 422
+			end
+		end
+
+		def destroy
+			post = Post.find(params[:id]
+			post.destroy
+			head 204
+		end
+
+		private
+
+			def post_params
+				params.require(:post).permit(:link, :user_id)
+			end
+		end
+
+end
