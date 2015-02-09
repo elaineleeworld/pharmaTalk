c1690ae0bef7637a163a6aa6383b24ffe3c64f97
Elaine Lee <elaneylee@gmail.com>
2015-02-08 17:14:28 -0800

    javascript controllers cookies and main

---
+++ app/assets/javascripts/controllers/maincontroller.js
@@ -0,0 +1,27 @@
+// IIFE
+// this controller is for before a person is logged in
+
+(function(){
+
+	angular
+		.module('pharmatalkapp')
+		.controller('MainController', MainController);
+
+
+	MainController.$inject = ['CommentsFactory', 'PostsFactory', ipCookie];
+
+	function MainController(CommentsFactory, PostsFactory, ipCookie){
+
+		var self = this;
+
+		// create PostFactory as an object
+		self.Post = new PostsFactory();
+		self.id = ipCookie('id');
+		// create CommentFactory as an object
+		self.comment = new CommentsFactory();
+		
+	}
+
+
+
+})();
