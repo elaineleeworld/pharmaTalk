5f209b3a6db9e67ab26ebb14a4ff6ac4b5684dc1
Elaine Lee <elaneylee@gmail.com>
2015-02-15 23:28:37 -0800

    login signup page styling

--- app/assets/javascripts/factories/tweetfactory.js
+++ app/assets/javascripts/factories/tweetfactory.js
@@ -35,5 +35,14 @@ TweetsFactory.$inject = ['Resources', '$http'];
 		// <td> {{tweet.text}}</td>
 		// </tr>
 		// </table>
+		// <!-- @client = Twitter::Streaming::Client.new do |config|
+          // config.consumer_key        = "YOUR_CONSUMER_KEY"
+          // config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+          // config.access_token        = "YOUR_ACCESS_TOKEN"
+          // config.access_token_secret = "YOUR_ACCESS_SECRET"
+        // end -->
+         // <%=# @client.sample do |tweet| %> 
+          // <%= tweet.text %> 
+        // <!-- <%#= end %> 
 
 	}
